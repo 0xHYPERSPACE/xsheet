5e9a3b03852c544f7151941f3f959a45ec1c6938
stm <stmataic@gmail.com>
2016-06-18 17:22:35 +0200

    putting data definitions back in

--- www/app/mock-sheets.ts
+++ www/app/mock-sheets.ts
@@ -1,14 +1,20 @@
 import { Sheet } from './sheet';
 
 export var SHEETS: Sheet[] = [
-  { 'id': 11, 'name': 'Mr. Nice' },
-  { 'id': 12, 'name': 'Narco' },
-  { 'id': 13, 'name': 'Bombasto' },
-  { 'id': 14, 'name': 'Celeritas' },
-  { 'id': 15, 'name': 'Magneta' },
-  { 'id': 16, 'name': 'RubberMan' },
-  { 'id': 17, 'name': 'Dynama' },
-  { 'id': 18, 'name': 'Dr IQ' },
-  { 'id': 19, 'name': 'Magma' },
-  { 'id': 20, 'name': 'Tornado' }
+  { 'id': 11, 'name': '#11',
+    'animator': 'Sinisa',
+    'scene': 28,
+    'sequence': 14,
+    'length': 48,
+    'production': 'Test',
+    'notes': 'Some notes here'},
+  { 'id': 12, 'name': '#12' },
+  { 'id': 13, 'name': '#13' },
+  { 'id': 14, 'name': '#14' },
+  { 'id': 15, 'name': '#15' },
+  { 'id': 16, 'name': '#16' },
+  { 'id': 17, 'name': '#17' },
+  { 'id': 18, 'name': '#18' },
+  { 'id': 19, 'name': '#19' },
+  { 'id': 20, 'name': '#20' }
 ];
